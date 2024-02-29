5fe5c6cf1eb482587ef776c7226a7b9722e1d89a
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-03-01 02:42:11 +0545

    hero section...

--- wp-content/themes/himalayaminds/header.php
+++ wp-content/themes/himalayaminds/header.php
@@ -14,6 +14,9 @@
     <meta charset="<?php bloginfo($charset) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> HimalayaMinds - Ascend to Excellence with HimalayaMinds. </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
 </head>
 
@@ -36,6 +39,51 @@
                 </ul>
             </nav>
         </div>
+
+        <section id="hero">
+            <div class="container intro">
+                <h1> Ascend to Excellence with HimalayaMinds. </h1>
+                <p>Join us and be a part of Himalaya Minds</p>
+            </div>
+            <div class="container">
+                <div class="himalayaminds-features">
+                    <ul class="hm-feature">
+                        <li class="feature-card">
+                            <div class="feature-icon">
+                                <i class="fas fa-users"></i>
+                            </div>
+                            <div class="feature-title">
+                                <span>Community</span>
+                            </div>
+                        </li>
+                        <li class="feature-card">
+                            <div class="feature-icon">
+                                <i class="fas fa-blog"></i>
+                            </div>
+                            <div class="feature-title">
+                                <span>Blog</span>
+                            </div>
+                        </li>
+                        <li class="feature-card">
+                            <div class="feature-icon">
+                                <i class="fas fa-server"></i>
+                            </div>
+                            <div class="feature-title">
+                                <span>Web Hosting</span>
+                            </div>
+                        </li>
+                        <li class="feature-card">
+                            <div class="feature-icon">
+                                <i class="fab fa-osi" style="font-weight: bold;"></i>
+                            </div>
+                            <div class="feature-title">
+                                <span>Open Source</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
 
 
 
